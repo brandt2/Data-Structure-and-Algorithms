ae6d9422284ac2e6a3bece4870e7cadfa63ce8fa
Brandon Tran <brandt2@uci.edu>
2019-04-11 16:07:23 -0700

    Work on coin change problem 2

--- memoization/memoization_project/lib/leet_code_518.js
+++ memoization/memoization_project/lib/leet_code_518.js
@@ -1,3 +1,20 @@
 // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
+function change(amount, coins, memo = {}) {
+  // one way to give back change
+  let key = amount + '-' + coins;
+  if (key in memo) return memo[key];
+  if (amount === 0) return 1;
+
+  let currentCoin = coins[coins.length - 1];
+
+  // this is the total number of times the amount reaches 0 with the coins we have
+  let total = 0;
+  for (let qty = 0; qty * currentCoin <= amount; qty++) {
+    total += change(amount - qty * currentCoin, coins.slice(0, -1), memo);
+  }
+
+  memo[key] = total;
+  return memo[key];
+}
