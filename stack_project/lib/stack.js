7ef9307f33f30df7f6252ee47bde89fb5a222e7e
Brandon Tran <brandt2@uci.edu>
2019-04-25 00:12:54 -0700

    Work on stack project

---
+++ stack_project/lib/stack.js
@@ -0,0 +1,72 @@
+// ============================================================================
+// Implementation Exercise: Stack
+// ============================================================================
+//
+// -------
+// Prompt:
+// -------
+//
+// Implement a Stack and all of its methods below!
+//
+// ------------
+// Constraints:
+// ------------
+//
+// Make sure the time and space complexity of each is equivalent to those 
+// in the table provided in the Time and Space Complexity Analysis section
+// of your Stack reading!
+//
+// -----------
+// Let's Code!
+// -----------
+
+class Node {
+  constructor(val){
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(){
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  push(val){
+    let newNode = new Node(val);
+    if (this.length === 0){
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      let temp = this.top;
+      this.top = newNode;
+      this.top.next = temp;
+    }
+    this.length++;
+    return this.length;
+  }
+
+  pop(){
+    let removedNode = this.top;
+    if (this.length === 0) {
+      return null;
+    } else if (this.length === 1) {
+      this.top = null;
+      this.bottom = null;
+    } else {
+      this.top = removedNode.next;
+    }
+    this.length--;
+    return removedNode.value;
+  }
+
+  size(){
+    return this.length;
+  }
+
+}
+
+exports.Node = Node;
+exports.Stack = Stack;
