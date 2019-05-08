da51453a825557c2e02553e7d58636d11747d24c
Brandon Tran <brandt2@uci.edu>
2019-05-07 18:28:02 -0700

    Work on bst leetcode problems

--- bst_project/lib/leet_code_108.js
+++ bst_project/lib/leet_code_108.js
@@ -3,5 +3,32 @@
 
 
 function sortedArrayToBST(nums) {
+  if (nums.length === 0) return null;
 
+  let midIdx;
+
+  if (nums.length % 2 === 0) {
+    midIdx = nums.length / 2;
+  } else {
+    midIdx = Math.floor(nums.length / 2);
+  }
+
+  const root = new TreeNode(nums[midIdx]);
+  root.left = sortedArrayToBST(nums.slice(0, midIdx));
+  root.right = sortedArrayToBST(nums.slice(midIdx+1));
+
+  return root;
 }
+
+// function sortedArrayToBST(nums) {
+//   if (!nums.length) return null;
+
+//   let midIdx = Math.floor(nums.length/2);
+//   let root = new TreeNode(nums[midIdx]);
+//   let leftSubtree = sortedArrayToBST(nums.slice(0, midIdx));
+//   let rightSubtree = sortedArrayToBST(nums.slice(midIdx + 1));
+//   root.left = leftSubtree;
+//   root.right = rightSubtree;
+
+//   return root;
+// }
--- bst_project/lib/leet_code_110.js
+++ bst_project/lib/leet_code_110.js
@@ -3,5 +3,27 @@
 
 
 function isBalanced(root) {
-
+  if (!root) return -1;
+  let heightDifference = Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1;
+  return heightDifference && isBalanced(root.left) && isBalanced(root.right);
 }
+
+function getHeight(root) {
+  if (!root) return -1;
+  return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+}
+
+// var isBalanced = function (root) {
+//   if (!root) return true;
+
+//   if (Math.abs(checkDepth(root.left) - checkDepth(root.right)) > 1) {
+//     return false;
+//   }
+
+//   return isBalanced(root.left) && isBalanced(root.right);
+// };
+
+// function checkDepth(root) {
+//   if (!root) return 0;
+//   return 1 + Math.max(checkDepth(root.left), checkDepth(root.right));
+// }
